db06dc90e4864bf23c29898393da6317967ea61e
laurent <laurent.mahieu@bisom.fr>
2023-07-07 11:38:23 +0200

    create home EssentialsView

--- src/views/sport/EssentialsView.vue
+++ src/views/sport/EssentialsView.vue
@@ -1,11 +1,37 @@
 <template>
-  <h1>EssentialsView</h1>
+  <v-row justify="center">
+    <v-card
+      width="300"
+      v-for="day in days"
+      :key="`day-${day.id}`"
+      class="ma-4 pointer"
+      @click="redirectTo(day.id)"
+    >
+      <v-card-title class="text-center">
+        {{ day.title }}
+      </v-card-title>
+    </v-card>
+  </v-row>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import ESSENTIAL_SESSIONS from "@/const/essentialsSessions";
+
 export default defineComponent({
   name: "EssentialsView",
+  data() {
+    return {
+      days: ESSENTIAL_SESSIONS,
+    };
+  },
+
+  methods: {
+    redirectTo(sessionId: number) {
+      this.$router.push(`/sport/essentials/day/${sessionId}`);
+      window.scrollTo(0, 0);
+    },
+  },
 });
 </script>
