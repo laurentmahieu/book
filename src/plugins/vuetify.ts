b5cbbf74e58a08080d6dc225a3c7ebc993f974f8
laurent <laurent.mahieu@bisom.fr>
2023-07-07 12:49:10 +0200

    add default props for textField

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,6 +4,14 @@ import { createVuetify } from "vuetify";
 import { aliases, fa } from "vuetify/iconsets/fa";
 
 export default createVuetify({
+  defaults: {
+    VTextField: {
+      variant: "outlined",
+      color: "primary",
+      density: "comfortable",
+      clearable: true,
+    },
+  },
   icons: {
     defaultSet: "fa",
     aliases,
